155148d96a44927322131bedf057331647bad426
Amine Aouragh <71039249+AmineAouragh@users.noreply.github.com>
2021-06-03 22:04:49 +0200

    Update MainActivity.java

--- app/src/main/java/com.example.contactsapp/MainActivity.java
+++ app/src/main/java/com.example.contactsapp/MainActivity.java
@@ -1,36 +1,75 @@
 package com.example.contactsapp;
-
+import android.Manifest;
 import android.app.Activity;
-
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import androidx.core.app.ActivityCompat;
 import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity {
-
-    EditText inputNumber;
-    Button make_call_btn;
+    private EditText inputNumber;
+    private Button btn_call;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         inputNumber = (EditText) findViewById(R.id.number);
-        make_call_btn = (Button) findViewById(R.id.btnCall);
+        btn_call = (Button) findViewById(R.id.btnCall);
 
-        make_call_btn.setOnClickListener(new OnClickListener() {
+        btn_call.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent callIntent = new Intent(Intent.ACTION_CALL);
-                callIntent.setData(Uri.parse("tel: " +inputNumber.getText().toString()));
-                startActivity(callIntent);
+                callPhoneNumber();
             }
         });
-
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 101) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                callPhoneNumber();
+            }
+        }
+    }
+
+    public void callPhoneNumber() {
+
+        try {
+
+            if (Build.VERSION.SDK_INT > 22) {
+
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) !=PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                            Manifest.permission.CALL_PHONE}, 101);
+                    return;
+                }
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + inputNumber.getText().toString()));
+                startActivity(callIntent);
+
+            } else {
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + inputNumber.getText().toString()));
+                startActivity(callIntent);
+            }
+            
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+    }
+
 }
