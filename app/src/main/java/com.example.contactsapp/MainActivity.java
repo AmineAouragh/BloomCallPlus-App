4b9993ccd9442a3c5af37de134c47efe910bb0de
Amine Aouragh <71039249+AmineAouragh@users.noreply.github.com>
2021-06-04 17:18:53 +0200

    Update MainActivity.java

--- app/src/main/java/com.example.contactsapp/MainActivity.java
+++ app/src/main/java/com.example.contactsapp/MainActivity.java
@@ -28,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        
+        getSupportActionBar().hide();
+        
         setContentView(R.layout.activity_main);
 
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
