3451d6393165bfb10d137fa73a7e10c74552b3af
Amine Aouragh <71039249+AmineAouragh@users.noreply.github.com>
2021-06-06 20:31:30 +0200

    Update and rename PlaceholderFragment.java to RecentsFragment.java

---
+++ app/src/main/java/com.example.contactsapp/main/RecentsFragment.java
@@ -0,0 +1,29 @@
+package com.example.contactsapp.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.contactsapp.R;
+
+
+public class RecentsFragment extends Fragment {
+
+    public RecentsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recents, container, false);
+    }
+
+}
