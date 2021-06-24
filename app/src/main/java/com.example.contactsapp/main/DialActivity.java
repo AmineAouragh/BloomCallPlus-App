f0827fc80bb08c0f9cf02611ee0b5f2f587b5974
Amine Aouragh <71039249+AmineAouragh@users.noreply.github.com>
2021-06-24 22:05:41 +0200

    Update DialActivity.java

--- app/src/main/java/com.example.contactsapp/main/DialActivity.java
+++ app/src/main/java/com.example.contactsapp/main/DialActivity.java
@@ -1,7 +1,9 @@
 package com.example.contactsapp.main;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,12 +12,17 @@ import com.example.contactsapp.R;
 
 public class DialActivity extends AppCompatActivity {
 
+    private final String[] numbers = {
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "0", "#"
+    };
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.grid_dialpad);
 
-        GridView gridView = (GridView) findViewById(R.id.dialpad_grid);
+        GridView gridView = (GridView) findViewById(R.id.grid_dial);
+        gridView.setAdapter(new TextViewAdapter(this, numbers));
 
 
     }
